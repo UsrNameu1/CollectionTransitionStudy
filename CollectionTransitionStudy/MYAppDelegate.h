8187d8b5e39dc3b967b8b2b232e98242096ffcc1
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-05-21 23:49:55 +0900

    Initial Commit

---
+++ CollectionTransitionStudy/MYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYAppDelegate.h
+//  CollectionTransitionStudy
+//
+//  Created by adachi yuichi on 2014/05/21.
+//  Copyright (c) 2014年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionTransitionStudy/MYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYViewController.h
+//  CollectionTransitionStudy
+//
+//  Created by adachi yuichi on 2014/05/21.
+//  Copyright (c) 2014年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYViewController : UIViewController
+
+@end
